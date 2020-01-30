ac6b5e01c88d7d676818871e8b8e14d62f01f2d8
Jordan Simmons <simmons12@mail.usf.edu>
2020-01-30 07:41:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+        }
+    }
+}
