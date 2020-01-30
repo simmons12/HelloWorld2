89a770d1f35d100783b8ca556fd9d44d16ef403d
Jordan Simmons <simmons12@mail.usf.edu>
2020-01-30 07:54:08 -0500

    Added One Piece conditional statement

--- Program.cs
+++ Program.cs
@@ -8,7 +8,28 @@ namespace HelloWorld2
         {
             Console.WriteLine("Hello World!");
 
-            
+            Console.WriteLine("Do you like One Piece? Who's your favorite character?");
+
+            string OnePiece = "Zoro";
+
+            Console.WriteLine(OnePiece);
+
+            if (OnePiece == "Zoro")
+            {
+                Console.WriteLine("Ah you like the swordsman! Pretty cool man.");
+            }
+            else if (OnePiece == "Luffy")
+            {
+                Console.WriteLine("Ah you're a fan of the captain, nice!");
+            }
+            else if (OnePiece == "Sanji")
+            {
+                Console.WriteLine("The cook is your favorite, huh? Interesting.");
+            }
+            else
+            {
+                Console.WriteLine("That's a weird choice. Just kidding!");
+            }
         }
     }
 }
